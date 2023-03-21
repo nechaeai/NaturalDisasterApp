1e37dedf1cc5918dfc8d1bd98f8e26e6699c6437
alekseynecha77 <nechaev.alexxx23@gmail.com>
2023-03-21 18:31:38 -0400

    update

--- src/main/java/com/example/demo/EarthquakeController.java
+++ src/main/java/com/example/demo/EarthquakeController.java
@@ -40,7 +40,7 @@ public class EarthquakeController {
     public String getEarthquakeData(@RequestParam String city, Model model) throws URISyntaxException {
         OkHttpClient client = new OkHttpClient();
 
-        URI uri = new URI("https", "earthquake.usgs.gov", "/fdsnws/event/1/query",
+        URI uri = new URI("http", "earthquake.usgs.gov", "/fdsnws/event/1/query",
                 "format=geojson&minmagnitude=5&limit=5&orderby=time&eventtype=earthquake&place=" + 
                 URLEncoder.encode(city, StandardCharsets.UTF_8).replace("+", "%20"), null);
         String url = uri.toASCIIString();
